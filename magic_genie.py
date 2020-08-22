5c92d533b2387a4f135f86ae93a94c3331c8418c
moseguera <63865874+moseguera@users.noreply.github.com>
2020-08-22 09:32:06 -0500

    Add files via upload

---
+++ magic_genie.py
@@ -0,0 +1,6 @@
+print("I am a genie.  You have three wishes")
+q="What would you like to wish for? "
+wish1=input(q)
+wish2=input(q)
+wish3=input(q)
+print("Your wishes are "+wish2+", "+wish3+", and "+wish1)
